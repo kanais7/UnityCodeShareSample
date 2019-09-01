6b930797ce845dcde836c8116afe712363240f1b
kanais_7 <kanais0620@gmail.com>
2019-09-01 18:49:35 +0900

    add preprocessor directives

--- unity/Assets/Scripts/Shared/Shared.cs
+++ unity/Assets/Scripts/Shared/Shared.cs
@@ -13,6 +13,14 @@ namespace Shared
         public static void DoSomething()
         {
             Console.WriteLine("do something");
+            
+            #if UNITY_2017_1_OR_NEWER
+            Console.WriteLine("execute in unity");
+            #endif
+            
+            #if NETCOREAPP
+            Console.WriteLine("execute in .netcoreapp");
+            #endif
         }
     }
 }
